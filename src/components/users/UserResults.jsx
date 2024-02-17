9ace89b8e59fd034ade542fc351dcca1b9f7d2d9
Aruna Turay <tura_arun@yahoo.com>
2024-02-17 15:59:19 -0500

    adds UsderItem, and to display each user

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 import { useEffect, useState } from 'react' //we want users to display when the page loads
 
 
@@ -29,7 +30,7 @@ if (!loading) {
     return (
         <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
           {users.map((user) => (
-            <h3>{user.login}</h3>
+            <UserItem key={user.id} user={user}/>
           ))}
         </div>
       )
